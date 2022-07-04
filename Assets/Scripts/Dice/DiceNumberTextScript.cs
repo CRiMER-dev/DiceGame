36bb06ff68ed65f50c7dbcbf1962c1e6c64ed5bf
CRiMER <CRiMER.dev@gmail.com>
2022-07-04 20:51:17 +0500

    Release 1 Dice

---
+++ Assets/Scripts/Dice/DiceNumberTextScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceNumberTextScript : MonoBehaviour {
+
+	TextMeshProUGUI text;
+	public static int diceNumber;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<TextMeshProUGUI> ();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        text.text = diceNumber.ToString();
+	}
+}
